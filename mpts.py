89083631874123782e1eac0dd986e230c9de07b9
Karol.Dudek <34249536+wojkd@users.noreply.github.com>
2018-05-10 22:47:13 +0200

    Add files via upload

--- mpts.py
+++ mpts.py
@@ -1,11 +1,13 @@
 from mpi4py import MPI
 import sys
 import time
+import os
 
 if (len(sys.argv) != 2):
     prime = 0
 else:
     prime = int(sys.argv[1])
+
 
 milli_sec = int(round(time.time() * 1000))
 comm = MPI.COMM_WORLD
@@ -15,6 +17,7 @@ proc = comm.Get_size()
 prime_range = int(primes/proc)
 diffrence = primes - prime_range*proc
 result = 0
+plik = open("result.txt", "w")
 if rank == 0:
     for i in range((prime_range*proc)+1, primes+1, 1):
         if ((i != 1) and (i != prime)): 
@@ -39,6 +42,10 @@ if rank == 0:
     if result == 1:
         print("number:", prime, "is not prime")
         print("execution time:", milli_sec, "milliseconds")
+        open("result.txt", "w").write("number: " + str(prime) + " is not prime" + 
+		"	execution time: " + str(milli_sec) + " milliseconds")
     else:
         print("number:", prime, "is prime")
         print("execution time:", milli_sec, "milliseconds")
+        open("result.txt", "w").write("number: " + str(prime) + " is prime" + 
+		"	execution time: " + str(milli_sec) + " milliseconds")
